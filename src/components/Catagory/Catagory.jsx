12b1947881200f7af296a006fdde0c85b523654e
Hipsss <pgermonenko@mail.ru>
2023-02-22 12:27:21 +0300

    DAs

--- src/components/Catagory/Catagory.jsx
+++ src/components/Catagory/Catagory.jsx
@@ -2,5 +2,34 @@ import React from "react";
 
 import "./Catagory.css";
 export default function Catagory() {
-  return <div className="catagoryContainer">{""}</div>;
+  return (
+    <div className="catagoryContainer">
+      <div className="catagoryItem">
+        <h3 className="catagoryH3">{"Bussiness"}</h3>
+        <p className="catagoryText">
+          {"Lorem ipsum dolor sit amet, consectetuer adipiscing elit."}
+        </p>
+      </div>
+      <div className="catagoryItem">
+        {" "}
+        {/*Добавить цвет  */}
+        <h3 className="catagoryH3">{"Startup"}</h3>
+        <p className="catagoryText">
+          {"Lorem ipsum dolor sit amet, consectetuer adipiscing elit."}
+        </p>
+      </div>
+      <div className="catagoryItem">
+        <h3 className="catagoryH3">{"Economy"}</h3>
+        <p className="catagoryText">
+          {"Lorem ipsum dolor sit amet, consectetuer adipiscing elit."}
+        </p>
+      </div>
+      <div className="catagoryItem">
+        <h3 className="catagoryH3">{"Technology"}</h3>
+        <p className="catagoryText">
+          {"Lorem ipsum dolor sit amet, consectetuer adipiscing elit."}
+        </p>
+      </div>
+    </div>
+  );
 }
